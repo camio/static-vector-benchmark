9d449f6ea6d7316d942bc6de27ec1a4d884aacc6
David Sankel <dsankel@adobe.com>
2023-06-15 17:13:22 +0300

    add make linux graphs script

---
+++ scripts/make_linux_graphs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+#
+# Utility script to create graphs of Linux runs
+
+./scripts/build_graph.sh --prefix Linux+Clang-17.0.0+Intel-Core-i7-118580H
+./scripts/build_graph.sh --prefix Linux+GCC-13.1.1+Intel-Core-i7-118580H
