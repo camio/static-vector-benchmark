a658113728cd6d4450d5ffe5e5c8cb4214c78ccc
Nevin Liber <nliber@anl.gov>
2023-06-15 17:17:39 +0300

    Adding the initial runs on an M2Max MacbookPro

---
+++ scripts/make_mac_runs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+#
+# Utility script to create Linux runs of the benchmark
+
+# sudo cpupower frequency-set -g performance
+
+make clean
+make CXX="clang++ -std=c++20"
+./scripts/multirun.sh --prefix Mac+AppleClang-14.0.3+M2Max
+
+# sudo cpupower frequency-set -g schedutil
